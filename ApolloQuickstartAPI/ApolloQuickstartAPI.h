1066fd76599e377a55340eeae97b714dbecdba65
Martijn Walraven <martijn@martijnwalraven.com>
2016-09-18 13:04:01 +0200

    Remove ApolloQuickstartAPI

--- ApolloQuickstartAPI/ApolloQuickstartAPI.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for ApolloQuickstartAPI.
-FOUNDATION_EXPORT double ApolloQuickstartAPIVersionNumber;
-
-//! Project version string for ApolloQuickstartAPI.
-FOUNDATION_EXPORT const unsigned char ApolloQuickstartAPIVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ApolloQuickstartAPI/PublicHeader.h>
